246f9d572c3892bc6c4c3553d9424584139feee4
郭一凡 <exonuclease57@gmail.com>
2016-03-24 18:20:44 +0800

    1.0

---
+++ tiebacrawlercs/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace tiebacrawlercs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(507, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始搜索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "帖子地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(447, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "楼层起始";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(577, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "楼层结束";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(507, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(647, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 21);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "关键字（逗号分开）";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(134, 54);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(288, 21);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(14, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(683, 318);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(518, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "反馈bug请联系771385071@qq.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 462);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "交易帖搜索器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ tiebacrawlercs/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using System.Collections;
+
+namespace tiebacrawlercs
+{
+    public partial class Form1 : Form
+    {
+        string url;
+        int pagestart;
+        int pageend;
+        List<string> keywords = new List<string>();
+        List<string> responsebodys = new List<string>();
+        List<HttpResponseMessage> ress = new List<HttpResponseMessage>();
+        Hashtable freplys = new Hashtable();
+
+        static bool fkw(List<string> kws, string html)
+        {
+            foreach (string kw in kws)
+            {
+                if (!(html.IndexOf(kw) > -1))
+                    return false;
+            }
+            return true;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.ReadOnly = true;
+            richTextBox1.DetectUrls = true;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim().Length < 7 || textBox1.Text.Trim().Substring(0, 7) != "http://")
+            {
+                MessageBox.Show("请输入正确的地址");
+                textBox1.Focus();
+            }
+            else
+                url = textBox1.Text.Trim();
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            Regex isnum = new Regex(@"\d+");
+            if (isnum.IsMatch(textBox2.Text.Trim()))
+            {
+                pagestart = Convert.ToInt32(textBox2.Text.Trim());
+            }
+            else
+            {
+                MessageBox.Show("请输入正确的起始楼层");
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            Regex isnum = new Regex(@"\d+");
+            if (isnum.IsMatch(textBox3.Text.Trim()))
+            {
+                pageend = Convert.ToInt32(textBox3.Text.Trim());
+            }
+            else
+            {
+                MessageBox.Show("请输入正确的结束楼层");
+                textBox3.Focus();
+            }
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            string[] keywordarray = textBox4.Text.Trim().Split(new Char[] { ',' });
+            keywords.Clear();
+            foreach (string keyword in keywordarray)
+            {
+                keywords.Add(keyword);
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            ress.Clear();
+            responsebodys.Clear();
+            richTextBox1.Clear();
+            freplys.Clear();
+            await createtask();
+        }
+
+        private async Task createtask()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/14.14291");
+            for (int i = pagestart; i <= pageend; i++)
+            {
+                ress.Add(await client.GetAsync(url + "?pn=" + i));
+            }
+            for (int i = 0; i <= pageend - pagestart; i++)
+            {
+                responsebodys.Add(await ress[i].Content.ReadAsStringAsync());
+            }
+            foreach (string responsebody in responsebodys)
+            {
+                Regex matchreply = new Regex(@"<div id=""post_content_\d*"" class=""d_post_content j_d_post_content "">.*?</div>");
+                MatchCollection matchedreplysc = matchreply.Matches(responsebody);
+                List<string> matchedreplys = new List<string>();
+                foreach (Match matchedreplyc in matchedreplysc)
+                {
+                    matchedreplys.Add(matchedreplyc.Value);
+                }
+                foreach (string matchedreply in matchedreplys)
+                {
+                    if (fkw(keywords, matchedreply))
+                    {
+                        Regex mreply = new Regex(@"(?<=>).*?(?=</div>)");
+                        Regex mpid = new Regex(@"(?<=post_content_).*?(?="")");
+                        string pid = mpid.Match(matchedreply).Value.Trim();
+                        string reply = mreply.Match(matchedreply).Value.Trim().Replace("<br>","\n");
+                        reply = Regex.Replace(reply,@"\n+","\n");
+                        try
+                        {
+                            freplys.Add(reply, pid);
+                        }
+                        catch(ArgumentException)
+                        {
+
+                        }
+                    }
+                }
+            }
+            foreach(string freply in freplys.Keys)
+            {
+                richTextBox1.Text += freply + "\n";
+                richTextBox1.Text += url + "?pid=" + freplys[freply] + "&cid=0#" + freplys[freply];
+                richTextBox1.Text += "\n\n";
+            }
+        }
+
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+    }
+}
